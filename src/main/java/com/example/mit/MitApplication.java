af719751310850cd0193f86711ca49bc1e20de79
kali <primat.2000@mail.ru>
2021-06-22 07:29:01 +0500

    Bug fixes

--- src/main/java/com/example/mit/MitApplication.java
+++ src/main/java/com/example/mit/MitApplication.java
@@ -3,6 +3,7 @@ package com.example.mit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
+
 @SpringBootApplication
 public class MitApplication {
 
